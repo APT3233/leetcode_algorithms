51502581781954c4b4af6fd3b0c226e5d791f0c9
APT3233 <khacanh19803535@gmail.com>
2024-08-25 01:08:34 +0700

    update problem 23 + delete solution2.cpp

---
+++ hard/23. Merge k Sorted Lists/apt/solution.c
@@ -0,0 +1,45 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+int cmp(const void* a, const void* b){
+    struct ListNode** nodeA = (struct ListNode**)a;
+    struct ListNode** nodeB = (struct ListNode**)b;
+    return (nodeA*)->val - (nodeB*)->val;
+}
+
+struct ListNode* mergeKLists(struct ListNode** lists, int listsSize){
+    if(listsSize == 0)  return NULL; 
+    struct ListNode** minHeap = (struct ListNode**)malloc(listsSize * sizeof(struct ListNode*))
+
+    int heapSize = 0;
+    for (int i = 0; i < listsSize; i++)
+        if(lists[i] != NULL)
+            minHeap[heapSize++] = lists[i];
+
+    qsort(minHeap, heapSize, sizeof(struct ListNode*), cmp);
+
+    struct ListNode dummy;
+    struct ListNode* tail = dummy;
+
+    while(heapSize > 0){
+        struct ListNode* node = minHeap[0];
+        minHeap[0] = minHeap[--heapSize];
+
+        qsort(minHeap, heapSize, sizeof(struct ListNode*), cmp);
+
+        tail->next = node;
+        tail = tail->next;
+
+        if(node->next != NULL){
+            minHeap[heapSize++] = node->next;
+            qsort(minHeap, heapSize, sizeof(struct ListNode*), cmp);
+        }
+        free(minHeap); 
+        return dummy.next;
+    }
+}
