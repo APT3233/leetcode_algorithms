576e96bb29bd218c09ca6c8605ed3407069c5693
NguyenDatGH <tandatbtqb@gmail.com>
2024-09-27 15:18:42 +0700

    handle push err

--- hard/23. Merge k Sorted Lists/rio/ListNode.java
+++ hard/23. Merge k Sorted Lists/rio/ListNode.java
@@ -1,13 +1,13 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
+
+//  Definition for singly-linked list.
+ public class ListNode {
+    int val;
+     ListNode next;
+    ListNode() {}
+     ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+
+ }
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
         if(lists == null || lists.length == 0){
